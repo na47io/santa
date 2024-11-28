5f3a400e58b18789d221cd42b7ff3e7363fdd57d
nikolay <alexandroff.nikolay05@gmail.com>
2024-11-28 12:39:34 +0000

    ci: deploy with db

--- app/database.py
+++ app/database.py
@@ -3,10 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, String, JSON, Integer, DateTime
 import datetime
-from uuid import UUID
-from typing import Optional
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./santa.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/santa.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -15,15 +13,20 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 class SessionStore(Base):
     __tablename__ = "sessions"
 
     id = Column(String, primary_key=True)  # UUID as string
     data = Column(JSON)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    updated_at = Column(
+        DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow
+    )
+
 
 Base.metadata.create_all(bind=engine)
+
 
 def get_db():
     db = SessionLocal()
